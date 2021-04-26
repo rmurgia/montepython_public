dc81a038a9523a9f199c744e82d04dd747f42717
riccardo.murgia@umontpellier.fr <riccardo.murgia@umontpellier.fr>
2021-04-26 12:40:58 +0200

    S8 and SPT likelihoods added

--- montepython/likelihoods/S8_COSEBI_DESKIDS/__init__.py
+++ montepython/likelihoods/S8_COSEBI_DESKIDS/__init__.py
@@ -2,7 +2,7 @@ import os
 from montepython.likelihood_class import Likelihood_prior
 
 
-class S8_DESY1(Likelihood_prior):
+class S8_COSEBI_DESKIDS(Likelihood_prior):
 
     # initialisation of the class is done within the parent Likelihood_prior. For
     # this case, it does not differ, actually, from the __init__ method in
